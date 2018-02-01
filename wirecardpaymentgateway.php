fa550aa5ac2502b7c0562ed6027d8fe07d4c8654
stroia eduard <eduard.stroia@wirecard.com>
2018-02-01 13:09:52 +0200

    #9 create backend basic configure form
    - Saving settings available
    - Examples provided

--- wirecardpaymentgateway.php
+++ wirecardpaymentgateway.php
@@ -39,12 +39,13 @@ class WirecardPaymentGateway extends PaymentModule
 {
     public function __construct()
     {
+        $this->config = $this->config();
         $this->name = 'wirecardpaymentgateway';
         $this->tab = 'payments_gateways';
-        $this->version = '0.0.1';
+        $this->version = '0.0.2';
         $this->author = 'Wirecard';
         $this->need_instance = 0;
-        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => '1.7.2.4');
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => "1.7.2.4");
         $this->bootstrap = true;
 
         parent::__construct();
@@ -69,4 +70,499 @@ class WirecardPaymentGateway extends PaymentModule
         }
         return true;
     }
+
+    /**
+     * available config params
+     *
+     * @since 0.0.2
+     *
+     * @return array
+     */
+    protected function config()
+    {
+        return array(
+
+            'basicdata' => array(
+                'tab' => $this->l('Access data'),
+                'fields' => array(
+                    array(
+                        'name' => 'configmode',
+                        'label' => $this->l('Configuration'),
+                        'type' => 'select',
+                        'default' => 'production',
+                        'required' => true,
+                        'options' => 'getConfigurationModes',
+                        'doc' => $this->l('For integration, select predefined configuration settings or \'Production\' for live systems'),
+                    ),
+                    array(
+                        'name' => 'customer_id',
+                        'label' => $this->l('Customer ID'),
+                        'type' => 'text',
+                        'default' => 'D200001',
+                        // 'maxchar' => 7,
+                        'required' => true,
+                        'sanitize' => 'trim',
+                        'doc' => $this->l('Customer number you received from Wirecard (customerId, i.e. D2#####).'),
+                        'docref' => 'https://guides.wirecard.at/request_parameters#customerid',
+                    ),
+                    array(
+                        'name' => 'send_shippingdata',
+                        'label' => $this->l('Forward consumer shipping data'),
+                        'default' => 1,
+                        'type' => 'onoff',
+                        'doc' => $this->l('Forwarding shipping data about your consumer to the respective financial service provider.')
+                    ),
+                    array(
+                        'name' => 'installment_billing_countries',
+                        'label' => $this->l('Allowed billing countries'),
+                        'type' => 'select',
+                        'multiple' => true,
+                        'size' => 10,
+                        'default' => array('AT', 'DE', 'CH'),
+                        'options' => 'getCountries',
+                        'group' => 'pt',
+                    )
+                )
+            ),
+
+        );
+    }
+
+    /**
+     * return options for configuration modes select
+     *
+     * @since 0.0.2
+     *
+     * @return array
+     */
+    private function getConfigurationModes()
+    {
+        return array(
+            array('key' => 'production', 'value' => $this->l('Production')),
+            array('key' => 'demo', 'value' => $this->l('Demo')),
+            array('key' => 'test', 'value' => $this->l('Test')),
+            array('key' => 'test3d', 'value' => $this->l('Test 3D'))
+        );
+    }
+
+    public function getContent()
+    {
+        //  $this->createTable();
+        $this->html = '<h2>' . $this->displayName . '</h2>';
+
+        if (Tools::isSubmit('btnSubmit')) {
+            $this->postValidation();
+            if (!count($this->postErrors)) {
+                $this->postProcess();
+            } else {
+                foreach ($this->postErrors as $err) {
+                    $this->html .= $this->displayError(html_entity_decode($err));
+                }
+            }
+        }
+
+        $context = $this->context;
+        $country = Tools::strtolower($context->country->iso_code);
+        $language = $context->language->iso_code;
+
+        if ($language != $country && $language = 'en') {
+            $language = 'en';
+        }
+
+        if (!in_array($country, array('gb', 'de', 'it', 'es', 'pl', 'nl', 'fr'))) {
+            $country = 'de';
+            $language = 'en';
+        }
+
+
+        $backendEnabled = Configuration::get('WCS_BASICDATA_BACKENDPW');
+        $this->context->smarty->assign(
+            array(
+                'country' => $country,
+                'language' => $language,
+                'shopversion' => _PS_VERSION_,
+                'pluginversion' => $this->version,
+                'is_core' => $this->isCore,
+                'module_dir' => $this->_path,
+                'link' => $this->context->link,
+                'backendEnabled' => $backendEnabled != null && Tools::strlen($backendEnabled) > 0,
+                'ajax_configtest_url' => $this->context->link->getAdminLink('AdminModules') . '&configure='
+                    . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name
+            )
+        );
+
+        $this->html .= $this->context->smarty->fetch(
+            dirname(__FILE__) . '/views/templates/admin/configuration.tpl'
+        );
+        $this->html .= $this->renderForm();
+
+        return $this->html;
+    }
+
+    /**
+     * render form
+     *
+     * @since 0.0.2
+     *
+     * @return string
+     */
+    private function renderForm()
+    {
+        $radio_type = 'switch';
+
+        $radio_options = array(
+            array(
+                'id' => 'active_on',
+                'value' => 1,
+                'label' => $this->l('Enabled')
+            ),
+            array(
+                'id' => 'active_off',
+                'value' => 0,
+                'label' => $this->l('Disabled')
+            )
+        );
+
+        $input_fields = array();
+        $tabs = array();
+        foreach ($this->config as $groupKey => $group) {
+            $tabs[$groupKey] = $this->l($group['tab']);
+            foreach ($group['fields'] as $f) {
+                $configGroup = isset($f['group']) ? $f['group'] : $groupKey;
+                if (isset($f['class'])) {
+                    $configGroup = 'pt';
+                }
+
+                $elem = array(
+                    'name' => $this->buildParamName($configGroup, $f['name']),
+                    'label' => $this->l($f['label']),
+                    'tab' => $groupKey,
+                    'type' => $f['type'],
+                    'required' => isset($f['required']) && $f['required']
+                );
+
+                if (isset($f['cssclass'])) {
+                    $elem['class'] = $f['cssclass'];
+                }
+
+                if (isset($f['doc'])) {
+                    if (is_array($f['doc'])) {
+                        $elem['desc'] = '';
+                        foreach ($f['doc'] as $d) {
+                            if (Tools::strlen($elem['desc'])) {
+                                $elem['desc'] .= '<br/>';
+                            }
+
+                            $elem['desc'] .= $this->l($d);
+                        }
+                    } else {
+                        $elem['desc'] = $this->l($f['doc']);
+                    }
+                }
+
+                if (isset($f['docref'])) {
+                    $elem['desc'] = isset($elem['desc']) ? $elem['desc'] . ' ' : '';
+                    $elem['desc'] .= sprintf(
+                        '<a target="_blank" href="%s">%s <i class="icon-external-link"></i></a>',
+                        $f['docref'],
+                        $this->l('More information')
+                    );
+                }
+
+                switch ($f['type']) {
+                    case 'text':
+                        if (!isset($elem['class'])) {
+                            $elem['class'] = 'fixed-width-xl';
+                        }
+
+                        if (isset($f['maxchar'])) {
+                            $elem['maxlength'] = $elem['maxchar'] = $f['maxchar'];
+                        }
+                        break;
+
+                    case 'onoff':
+                        $elem['type'] = $radio_type;
+                        $elem['class'] = 't';
+                        $elem['is_bool'] = true;
+                        $elem['values'] = $radio_options;
+                        break;
+
+                    case 'select':
+                        if (isset($f['multiple'])) {
+                            $elem['multiple'] = $f['multiple'];
+                        }
+
+                        if (isset($f['size'])) {
+                            $elem['size'] = $f['size'];
+                        }
+
+                        if (isset($f['options'])) {
+                            $optfunc = $f['options'];
+                            $options = array();
+                            if (is_array($optfunc)) {
+                                $options = $optfunc;
+                            }
+
+                            if (method_exists($this, $optfunc)) {
+                                $options = $this->$optfunc();
+                            }
+
+                            $elem['options'] = array(
+                                'query' => $options,
+                                'id' => 'key',
+                                'name' => 'value'
+                            );
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+                $input_fields[] = $elem;
+            }
+        }
+
+        $fields_form_settings = array(
+            'form' => array(
+                'tabs' => $tabs,
+                'legend' => array(
+                    'title' => $this->l('Settings'),
+                    'icon' => 'icon-cogs'
+                ),
+                'input' => $input_fields,
+                'submit' => array(
+                    'title' => $this->l('Save')
+                )
+            ),
+        );
+
+
+        /** @var HelperFormCore $helper */
+        $helper = new HelperForm();
+        $helper->show_toolbar = false;
+
+        /** @var LanguageCore $lang */
+        $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
+        $helper->default_form_language = $lang->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get(
+            'PS_BO_ALLOW_EMPLOYEE_FORM_LANG'
+        ) : 0;
+        $helper->id = (int)Tools::getValue('id_carrier');
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'btnSubmit';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFieldsValues(),
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+            'ajax_configtest_url' => $this->context->link->getAdminLink('AdminModules') . '&configure=' . $this->name
+                . '&tab_module=' . $this->tab . '&module_name=' . $this->name
+        );
+
+        return $helper->generateForm(array($fields_form_settings));
+    }
+
+    /**
+     * build prestashop internal parameter name
+     *
+     * @since 0.0.2
+     *
+     * @param $group
+     * @param $name
+     *
+     * @return string
+     */
+    protected function buildParamName($group, $name)
+    {
+        return sprintf(
+            'WDEE_%s_%s',
+            Tools::strtoupper($group),
+            Tools::strtoupper($name)
+        );
+    }
+
+    /**
+     * return saved config parameter values
+     *
+     * @since 0.0.2
+     *
+     * @return array
+     */
+    public function getConfigFieldsValues()
+    {
+        $values = array();
+        foreach ($this->getAllConfigurationParameters() as $parameter) {
+            $val = Configuration::get($parameter['param_name']);
+            if (isset($parameter['multiple']) && $parameter['multiple']) {
+                if (!is_array($val)) {
+                    $val = Tools::strlen($val) ? Tools::jsonDecode($val) : array();
+                }
+
+                $x = array();
+                foreach ($val as $v) {
+                    $x[$v] = $v;
+                }
+                $pname = $parameter['param_name'] . '[]';
+                $values[$pname] = $x;
+            } else {
+                $values[$parameter['param_name']] = $val;
+            }
+        }
+
+        return $values;
+    }
+
+    /**
+     * return alls configuration parameters
+     *
+     * @since 0.0.2
+     *
+     * @return array
+     */
+    public function getAllConfigurationParameters()
+    {
+        $params = array();
+        foreach ($this->config as $groupKey => $group) {
+            foreach ($group['fields'] as $f) {
+                $configGroup = isset($f['group']) ? $f['group'] : $groupKey;
+
+                if (isset($f['class'])) {
+                    $configGroup = 'pt';
+                }
+
+                $f['param_name'] = $this->buildParamName(
+                    $configGroup,
+                    $f['name']
+                );
+                $params[] = $f;
+            }
+        }
+
+        return $params;
+    }
+
+    /**
+     * return available country iso codes
+     *
+     * @since 0.0.2
+     *
+     * @return array
+     */
+    protected function getCountries()
+    {
+        $cookie = $this->context->cookie;
+        $countries = Country::getCountries($cookie->id_lang);
+        $ret = array();
+        foreach ($countries as $country) {
+            $ret[] = array(
+                'key' => $country['iso_code'],
+                'value' => $country['name']
+            );
+        }
+
+        return $ret;
+    }
+
+    /**
+     * validate post parameters
+     *
+     * @since 0.0.2
+     *
+     */
+    private function postValidation()
+    {
+        if (Tools::isSubmit('btnSubmit')) {
+            $configmode = Tools::getValue('WCS_BASICDATA_CONFIGMODE');
+
+            foreach ($this->getAllConfigurationParameters() as $parameter) {
+                $val = Tools::getValue($parameter['param_name']);
+
+                if (isset($parameter['sanitize'])) {
+                    switch ($parameter['sanitize']) {
+                        case 'trim':
+                            $val = trim($val);
+                            break;
+                    }
+                }
+
+                if (isset($parameter['required']) && $parameter['required'] && !Tools::strlen($val)) {
+                    if (in_array(
+                        $parameter['name'],
+                        array(
+                            'customer_id',
+                            'shop_id',
+                            'secret',
+                            'backendpw'
+                        )
+                    )) {
+                        if ($configmode == 'production') {
+                            $this->postErrors[] = $parameter['label'] . ' ' . $this->l('is required.');
+                        }
+                    } else {
+                        $this->postErrors[] = $parameter['label'] . ' ' . $this->l('is required.');
+                    }
+                }
+
+                /* if ($parameter['name'] == 'customer_id') {
+                     preg_match("/^D2[0-8]\d{4}|9[5-9]\d{3}$/", $val, $output);
+                     if (count($output) !== 1) {
+                         $this->postErrors[] = $this->l('Incorrect customer ID.');
+                     }
+                 }*/
+
+                if (!isset($parameter['validator'])) {
+                    continue;
+                }
+
+                switch ($parameter['validator']) {
+                    case 'numeric':
+                        if (Tools::strlen($val) && !is_numeric($val)) {
+                            $this->postErrors[] = $parameter['label'] . ' ' . $this->l(' must be a number.');
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    /**
+     * process form post
+     *
+     * @since 0.0.2
+     *
+     */
+    private function postProcess()
+    {
+        if (Tools::isSubmit('ajax')) {
+            if (Tools::getValue('action') == 'ajaxTestConfig') {
+                $this->ajaxTestConfig();
+            }
+        } else {
+            if (Tools::isSubmit('btnSubmit')) {
+                foreach ($this->getAllConfigurationParameters() as $parameter) {
+                    $val = Tools::getValue($parameter['param_name']);
+
+                    if (isset($parameter['sanitize'])) {
+                        switch ($parameter['sanitize']) {
+                            case 'trim':
+                                $val = trim($val);
+                                break;
+                        }
+                    }
+
+                    if (is_array($val)) {
+                        $val = Tools::jsonEncode($val);
+                    }
+
+                    Configuration::updateValue($parameter['param_name'], $val);
+                }
+            }
+            $this->html .= $this->displayConfirmation($this->l('Settings updated'));
+        }
+    }
+
 }
