555c64c3ddcf0fcb8127fc479604010ddfcb712e
stroia eduard <eduard.stroia@wirecard.com>
2018-02-27 09:13:56 +0200

    #32 autoload added

---
+++ wirecardpaymentgateway/vendor/wirecardee_autoload.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
+
+
+spl_autoload_register('wirecardee_autoload');
+
+function wirecardee_autoload($class)
+{
+    $namespaces = array( 'Wirecard');
+    $namespace = null;
+    $modelNamespace = 'WirecardPaymentGateway';
+    $paymentNamespace = 'WirecardPaymentGatewayPayment';
+
+    foreach ($namespaces as $ns) {
+
+        if (strncmp($ns, $class, Tools::strlen($ns)) !== 0) {
+            continue;
+        } else {
+            $namespace = $ns;
+            break;
+        }
+    }
+    if ($namespace === null) {
+        return;
+    }
+
+    if (strcmp($class, $modelNamespace) > 0) {
+        $classWithUnderscore = 'Wirecard_PaymentGateway_';
+        if ((strcmp($paymentNamespace, Tools::substr($class, Tools::strlen($paymentNamespace))) >= 0)
+            && ((Tools::substr($class, Tools::strlen($paymentNamespace))) != '')
+        ) {
+            $classWithUnderscore .= 'Payment_' . Tools::substr($class, Tools::strlen($paymentNamespace));
+        } else {
+            $classWithUnderscore .= Tools::substr($class, Tools::strlen($modelNamespace));
+        }
+        $class = $classWithUnderscore;
+    }
+
+    $file = str_replace(array('\\', '_'), '/', $class) . '.php';
+
+    require_once $file;
+}
+
